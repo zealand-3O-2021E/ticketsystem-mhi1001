ea2d71896ce321540de6f8922e719e72084f0856
pc <pc>
2021-09-23 20:15:57 +0200

    task2

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+
+
+    public class Car
+    {
+
+        /// <summary>
+        /// Car Constructor that takes two parameters
+        /// </summary>
+        /// <param name="licensePlate">This is the license plate of the object</param>
+        /// <param name="date">This is the date of creation or registration</param>
+        public Car(string licensePlate, DateTime date)
+        {
+            LicensePlate = licensePlate;
+            Date = date;
+        }
+
+
+        public string LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Method that can be called to know the price of a ticket for the object
+        /// </summary>
+        /// <returns>A double, which is the value of the ticket for this object</returns>
+        public double Price()
+        {
+            return 240;
+        }
+        /// <summary>
+        /// Returns the type of vehicle
+        /// </summary>
+        /// <returns>Returns a string with the type of the vehicle</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+
+        
+    }
+}
---
+++ ClassLibraryTicketSystemTests1/CarTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        /// <summary>
+        /// Tests if the car method returns its designated price which is 240.
+        /// </summary>
+        [TestMethod()]
+        public void PriceTest()
+        {
+            //Arrange
+            Car c1 = new Car("32ha", DateTime.Now);
+            //Act
+            double actualPrice = c1.Price();
+            //Assert
+            Assert.AreEqual(240,actualPrice);
+        }
+
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //Arrange
+            Car c1 = new Car("32ha", DateTime.Now);
+            //Act
+            string actualType = c1.VehicleType();
+            //Assert
+            Assert.AreEqual("Car", actualType);
+        }
+    }
+}
