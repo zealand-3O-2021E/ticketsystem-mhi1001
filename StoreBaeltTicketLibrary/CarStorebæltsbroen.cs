9d44aa019e5b40264ceed36be05d73eb6d845c53
pc <pc>
2021-09-27 19:40:50 +0200

    task 9

---
+++ StoreBaeltTicketLibrary/CarStorebæltsbroen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ClassLibraryTicketSystem;
+
+namespace StoreBaeltTicketLibrary
+{
+    /// <summary>
+    /// Class for the vehicles that go through the Oresundbron ticketing system
+    /// </summary>
+    public class CarStorebæltsbroen : Vehicle
+    {
+        public CarStorebæltsbroen(string licensePlate, DateTime date) : base(licensePlate, date)
+        {
+            Date = date;
+            LicensePlate = licensePlate;
+        }
+        /// <summary>
+        /// Returns the price depending if they went through the bridge on a weekday or weekend and if they used brobizz
+        /// </summary>
+        /// <returns>A double with the ticket price amount</returns>
+        public override double Price()
+        {
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                if (BrobizzUsed)
+                {
+
+                    double weekendDiscount = 240 - (240 * 0.2);
+                    weekendDiscount = +weekendDiscount - (weekendDiscount * 0.05);
+                    return weekendDiscount;
+                }
+                return 240 - (240 * 0.2);
+            }
+
+            if (BrobizzUsed)
+            {
+                return 240 - (240 * 0.05);
+            }
+
+            return 240;
+        }
+        /// <summary>
+        /// Car type
+        /// </summary>
+        /// <returns>String with the car type</returns>
+        public override string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ StoreBaeltTicketLibraryTests/CarStorebæltsbroenTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary.Tests
+{
+    [TestClass()]
+    public class CarStorebæltsbroenTests
+    {
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod()]
+        public void CarStorebæltsbroenLicensePlateArgumentExceptionTest()
+        {
+            //Arrange & Act
+            CarStorebæltsbroen c1 = new CarStorebæltsbroen("12345678", DateTime.Now);
+
+            //Assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CarStorebæltsbroenWeekendWITHBroBizzPriceTest()
+        {
+            //Arrange
+            CarStorebæltsbroen c1 = new CarStorebæltsbroen("32ha", DateTime.Parse("26/09/2021"));
+            //Act
+            c1.BrobizzUsed = true;
+            double actualPrice = c1.Price();
+
+            //Assert -> Weekend discount = 48 so 240-48=192 then apply bro bizz discount 192-9.6 = 182.4
+
+            Assert.AreEqual(182.4, actualPrice, 0.01);
+        }
+        [TestMethod()]
+        public void CarStorebæltsbroenWeekendPriceTest()
+        {
+            //Arrange
+            CarStorebæltsbroen c1 = new CarStorebæltsbroen("32ha", DateTime.Parse("26/09/2021"));
+            //Act
+            c1.BrobizzUsed = false;
+            double actualPrice = c1.Price();
+
+            //Assert
+
+            Assert.AreEqual(192, actualPrice, 0.01);
+        }
+        [TestMethod()]
+        public void CarStorebæltsbroenWithBroBizz()
+        {
+            //Arrange
+            CarStorebæltsbroen c1 = new CarStorebæltsbroen("32ha", DateTime.Parse("28/09/2021"));
+            //Act
+            c1.BrobizzUsed = true;
+            double actualPrice = c1.Price();
+
+            //Assert 
+            Assert.AreEqual(228, actualPrice, 0.01);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //Arrange
+            CarStorebæltsbroen c1 = new CarStorebæltsbroen("32ha", DateTime.Now);
+            //Act
+            string actualType = c1.VehicleType();
+            //Assert
+            Assert.AreEqual("Car", actualType);
+        }
+    }
+}
